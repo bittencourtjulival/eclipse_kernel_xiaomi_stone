unknownbaka/utopia_kernel_polaris

qcacld-3.0: Add function to handle the FTM time sync events from target
Add the function to parse the FTM timesync start/stop and offset
events received from target and populate them in ftm timsync
priv vdev structure.

Change-Id: Ibdcf97bb0a04fc6490c6bd2c26701036df92c5d4
CRs-Fixed: 2618646

components/ftm_time_sync/core/inc/ftm_time_sync_main.h
@@ -26,6 +26,7 @@
 #define _FTM_TIME_SYNC_MAIN_H_
 
 #include <qdf_types.h>
+#include <qdf_defer.h>
 #include "ftm_time_sync_priv.h"
 #include "ftm_time_sync_objmgr.h"
 

components/ftm_time_sync/core/src/ftm_time_sync_main.c
@@ -20,11 +20,63 @@
  */
 
 #include "ftm_time_sync_main.h"
+#include "target_if_ftm_time_sync.h"
+#include <pld_common.h>
+
+static void ftm_time_sync_work_handler(void *arg)
+{
+	struct ftm_timesync_vdev_priv *vdev_priv = arg;
+	struct wlan_objmgr_psoc *psoc;
+	qdf_device_t qdf_dev;
+	QDF_STATUS status;
+	uint8_t vdev_id;
+	uint64_t lpass_ts;
+
+	if (!vdev_priv) {
+		ftm_time_sync_err("ftm vdev priv is Null");
+		return;
+	}
+
+	psoc = wlan_vdev_get_psoc(vdev_priv->vdev);
+	if (!psoc) {
+		ftm_time_sync_err("Failed to get psoc");
+		return;
+	}
+
+	vdev_id = wlan_vdev_get_id(vdev_priv->vdev);
+
+	qdf_dev = wlan_psoc_get_qdf_dev(psoc);
+	pld_get_audio_wlan_timestamp(qdf_dev->dev, PLD_TRIGGER_NEGATIVE_EDGE,
+				     &lpass_ts);
+
+	qdf_mutex_acquire(&vdev_priv->ftm_time_sync_mutex);
+
+	if (vdev_priv->num_reads) {
+		vdev_priv->num_reads--;
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+		qdf_sched_delayed_work(&vdev_priv->ftm_time_sync_work,
+				       vdev_priv->time_sync_interval);
+	} else {
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+	}
+
+	if (vdev_priv->valid) {
+		status = vdev_priv->tx_ops.ftm_time_sync_send_qtime(
+						psoc, vdev_id, lpass_ts);
+		if (status != QDF_STATUS_SUCCESS)
+			ftm_time_sync_err("send_ftm_time_sync_qtime failed %d",
+					  status);
+		vdev_priv->valid = false;
+	} else {
+		vdev_priv->valid = true;
+	}
+}
 
 QDF_STATUS
 ftm_timesync_vdev_create_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 {
 	struct ftm_timesync_vdev_priv *vdev_priv;
+	struct wlan_objmgr_psoc *psoc;
 	QDF_STATUS status;
 
 	vdev_priv = qdf_mem_malloc(sizeof(*vdev_priv));
@@ -41,13 +93,48 @@ ftm_timesync_vdev_create_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 		goto free_vdev_priv;
 	}
 
+	psoc = wlan_vdev_get_psoc(vdev);
+	if (!psoc) {
+		ftm_time_sync_err("Failed to get psoc");
+		return QDF_STATUS_E_INVAL;
+	}
+
 	vdev_priv->vdev = vdev;
+
+	qdf_create_delayed_work(&vdev_priv->ftm_time_sync_work,
+				ftm_time_sync_work_handler, vdev_priv);
+
+	qdf_mutex_create(&vdev_priv->ftm_time_sync_mutex);
+	target_if_ftm_time_sync_register_tx_ops(&vdev_priv->tx_ops);
+	target_if_ftm_time_sync_register_rx_ops(&vdev_priv->rx_ops);
+
+	vdev_priv->rx_ops.ftm_timesync_register_start_stop(psoc);
+	vdev_priv->rx_ops.ftm_timesync_regiser_master_slave_offset(psoc);
+
+	vdev_priv->valid = true;
+
 	goto exit;
 
 free_vdev_priv:
 	qdf_mem_free(vdev_priv);
 	status = QDF_STATUS_E_INVAL;
 exit:
+	return status;
+}
+
+static QDF_STATUS
+ftm_time_sync_deregister_wmi_events(struct wlan_objmgr_vdev *vdev)
+{
+	struct wlan_objmgr_psoc *psoc;
+	QDF_STATUS status;
+
+	psoc = wlan_vdev_get_psoc(vdev);
+	if (!psoc) {
+		ftm_time_sync_err("Failed to get psoc");
+		return QDF_STATUS_E_INVAL;
+	}
+
+	status = target_if_ftm_time_sync_unregister_ev_handlers(psoc);
 	return status;
 }
 
@@ -62,6 +149,11 @@ ftm_timesync_vdev_destroy_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 		ftm_time_sync_err("vdev priv is NULL");
 		goto exit;
 	}
+
+	qdf_mutex_destroy(&vdev_priv->ftm_time_sync_mutex);
+	qdf_cancel_delayed_work(&vdev_priv->ftm_time_sync_work);
+
+	ftm_time_sync_deregister_wmi_events(vdev);
 
 	status = wlan_objmgr_vdev_component_obj_detach(
 					vdev, WLAN_UMAC_COMP_FTM_TIME_SYNC,

components/ftm_time_sync/dispatcher/src/wlan_ftm_time_sync_tgt_api.c
@@ -1,0 +1,103 @@
+/*
+ *Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * DOC: Implements public API for ftm time sync to interact with target/WMI
+ */
+
+#include "wlan_ftm_time_sync_tgt_api.h"
+#include "ftm_time_sync_main.h"
+#include "wlan_ftm_time_sync_public_struct.h"
+#include <wmi_unified_api.h>
+
+QDF_STATUS
+tgt_ftm_ts_start_stop_evt(struct wlan_objmgr_psoc *psoc,
+			  struct ftm_time_sync_start_stop_params *param)
+{
+	struct wlan_objmgr_vdev *vdev;
+	struct ftm_timesync_vdev_priv *vdev_priv;
+	uint8_t vdev_id;
+
+	vdev_id = param->vdev_id;
+
+	vdev = wlan_objmgr_get_vdev_by_id_from_psoc(psoc, vdev_id,
+						    FTM_TIME_SYNC_ID);
+	if (!vdev) {
+		ftm_time_sync_err("failed to get vdev");
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	vdev_priv = ftm_timesync_vdev_get_priv(vdev);
+
+	qdf_mutex_acquire(&vdev_priv->ftm_time_sync_mutex);
+
+	vdev_priv->time_sync_interval = param->timer_interval;
+	vdev_priv->num_reads = param->num_reads * 2;
+
+	if (vdev_priv->num_reads && vdev_priv->time_sync_interval) {
+		vdev_priv->num_reads--;
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+		qdf_sched_delayed_work(&vdev_priv->ftm_time_sync_work,
+				       param->timer_interval);
+	} else if (vdev_priv->time_sync_interval == 0) {
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+		vdev_priv->ftm_ts_priv.qtime_ref = param->qtime;
+		vdev_priv->ftm_ts_priv.mac_ref = param->mac_time;
+		qdf_cancel_delayed_work(&vdev_priv->ftm_time_sync_work);
+	} else {
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+	}
+
+	ftm_timesync_vdev_put_ref(vdev);
+
+	return QDF_STATUS_SUCCESS;
+}
+
+QDF_STATUS tgt_ftm_ts_offset_evt(struct wlan_objmgr_psoc *psoc,
+				 struct ftm_time_sync_offset *param)
+{
+	struct ftm_timesync_vdev_priv *vdev_priv;
+	struct wlan_objmgr_vdev *vdev;
+	uint8_t vdev_id;
+	int iter;
+
+	vdev_id = param->vdev_id;
+
+	vdev = wlan_objmgr_get_vdev_by_id_from_psoc(psoc, vdev_id,
+						    FTM_TIME_SYNC_ID);
+	if (!vdev) {
+		ftm_time_sync_err("failed to get vdev");
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	vdev_priv = ftm_timesync_vdev_get_priv(vdev);
+
+	vdev_priv->num_qtime_pair = param->num_qtime <
+			FTM_TIME_SYNC_QTIME_PAIR_MAX ? param->num_qtime :
+			FTM_TIME_SYNC_QTIME_PAIR_MAX;
+
+	for (iter = 0; iter < vdev_priv->num_qtime_pair; iter++) {
+		vdev_priv->ftm_ts_priv.time_pair[iter].qtime_master =
+						param->pairs[iter].qtime_master;
+		vdev_priv->ftm_ts_priv.time_pair[iter].qtime_slave =
+						param->pairs[iter].qtime_slave;
+	}
+
+	ftm_timesync_vdev_put_ref(vdev);
+
+	return QDF_STATUS_SUCCESS;
+}
+

bittencourtjulival/eclipse_kernel_xiaomi_stone

components/ftm_time_sync/core/inc/ftm_time_sync_main.h
@@ -26,6 +26,7 @@
 #define _FTM_TIME_SYNC_MAIN_H_
 
 #include <qdf_types.h>
+#include <qdf_delayed_work.h>
 #include "ftm_time_sync_priv.h"
 #include "ftm_time_sync_objmgr.h"
 

components/ftm_time_sync/core/src/ftm_time_sync_main.c
@@ -20,11 +20,63 @@
  */
 
 #include "ftm_time_sync_main.h"
+#include "target_if_ftm_time_sync.h"
+#include <pld_common.h>
+
+static void ftm_time_sync_work_handler(void *arg)
+{
+	struct ftm_timesync_vdev_priv *vdev_priv = arg;
+	struct wlan_objmgr_psoc *psoc;
+	qdf_device_t qdf_dev;
+	QDF_STATUS status;
+	uint8_t vdev_id;
+	uint64_t lpass_ts;
+
+	if (!vdev_priv) {
+		ftm_time_sync_err("ftm vdev priv is Null");
+		return;
+	}
+
+	psoc = wlan_vdev_get_psoc(vdev_priv->vdev);
+	if (!psoc) {
+		ftm_time_sync_err("Failed to get psoc");
+		return;
+	}
+
+	vdev_id = wlan_vdev_get_id(vdev_priv->vdev);
+
+	qdf_dev = wlan_psoc_get_qdf_dev(psoc);
+	pld_get_audio_wlan_timestamp(qdf_dev->dev, PLD_TRIGGER_NEGATIVE_EDGE,
+				     &lpass_ts);
+
+	qdf_mutex_acquire(&vdev_priv->ftm_time_sync_mutex);
+
+	if (vdev_priv->num_reads) {
+		vdev_priv->num_reads--;
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+		qdf_delayed_work_start(&vdev_priv->ftm_time_sync_work,
+				       vdev_priv->time_sync_interval);
+	} else {
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+	}
+
+	if (vdev_priv->valid) {
+		status = vdev_priv->tx_ops.ftm_time_sync_send_qtime(
+						psoc, vdev_id, lpass_ts);
+		if (status != QDF_STATUS_SUCCESS)
+			ftm_time_sync_err("send_ftm_time_sync_qtime failed %d",
+					  status);
+		vdev_priv->valid = false;
+	} else {
+		vdev_priv->valid = true;
+	}
+}
 
 QDF_STATUS
 ftm_timesync_vdev_create_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 {
 	struct ftm_timesync_vdev_priv *vdev_priv;
+	struct wlan_objmgr_psoc *psoc;
 	QDF_STATUS status;
 
 	vdev_priv = qdf_mem_malloc(sizeof(*vdev_priv));
@@ -41,13 +93,51 @@ ftm_timesync_vdev_create_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 		goto free_vdev_priv;
 	}
 
+	psoc = wlan_vdev_get_psoc(vdev);
+	if (!psoc) {
+		ftm_time_sync_err("Failed to get psoc");
+		return QDF_STATUS_E_INVAL;
+	}
+
 	vdev_priv->vdev = vdev;
+	status = qdf_delayed_work_create(&vdev_priv->ftm_time_sync_work,
+					 ftm_time_sync_work_handler, vdev_priv);
+	if (QDF_IS_STATUS_ERROR(status)) {
+		ftm_time_sync_err("Failed to create ftm time sync work\n");
+		goto free_vdev_priv;
+	}
+
+	qdf_mutex_create(&vdev_priv->ftm_time_sync_mutex);
+	target_if_ftm_time_sync_register_tx_ops(&vdev_priv->tx_ops);
+	target_if_ftm_time_sync_register_rx_ops(&vdev_priv->rx_ops);
+
+	vdev_priv->rx_ops.ftm_timesync_register_start_stop(psoc);
+	vdev_priv->rx_ops.ftm_timesync_regiser_master_slave_offset(psoc);
+
+	vdev_priv->valid = true;
+
 	goto exit;
 
 free_vdev_priv:
 	qdf_mem_free(vdev_priv);
 	status = QDF_STATUS_E_INVAL;
 exit:
+	return status;
+}
+
+static QDF_STATUS
+ftm_time_sync_deregister_wmi_events(struct wlan_objmgr_vdev *vdev)
+{
+	struct wlan_objmgr_psoc *psoc;
+	QDF_STATUS status;
+
+	psoc = wlan_vdev_get_psoc(vdev);
+	if (!psoc) {
+		ftm_time_sync_err("Failed to get psoc");
+		return QDF_STATUS_E_INVAL;
+	}
+
+	status = target_if_ftm_time_sync_unregister_ev_handlers(psoc);
 	return status;
 }
 
@@ -62,6 +152,11 @@ ftm_timesync_vdev_destroy_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 		ftm_time_sync_err("vdev priv is NULL");
 		goto exit;
 	}
+
+	qdf_mutex_destroy(&vdev_priv->ftm_time_sync_mutex);
+	qdf_delayed_work_destroy(&vdev_priv->ftm_time_sync_work);
+
+	ftm_time_sync_deregister_wmi_events(vdev);
 
 	status = wlan_objmgr_vdev_component_obj_detach(
 					vdev, WLAN_UMAC_COMP_FTM_TIME_SYNC,

components/ftm_time_sync/dispatcher/src/wlan_ftm_time_sync_tgt_api.c
@@ -1,0 +1,103 @@
+/*
+ *Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * DOC: Implements public API for ftm time sync to interact with target/WMI
+ */
+
+#include "wlan_ftm_time_sync_tgt_api.h"
+#include "ftm_time_sync_main.h"
+#include "wlan_ftm_time_sync_public_struct.h"
+#include <wmi_unified_api.h>
+
+QDF_STATUS
+tgt_ftm_ts_start_stop_evt(struct wlan_objmgr_psoc *psoc,
+			  struct ftm_time_sync_start_stop_params *param)
+{
+	struct wlan_objmgr_vdev *vdev;
+	struct ftm_timesync_vdev_priv *vdev_priv;
+	uint8_t vdev_id;
+
+	vdev_id = param->vdev_id;
+
+	vdev = wlan_objmgr_get_vdev_by_id_from_psoc(psoc, vdev_id,
+						    FTM_TIME_SYNC_ID);
+	if (!vdev) {
+		ftm_time_sync_err("failed to get vdev");
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	vdev_priv = ftm_timesync_vdev_get_priv(vdev);
+
+	qdf_mutex_acquire(&vdev_priv->ftm_time_sync_mutex);
+
+	vdev_priv->time_sync_interval = param->timer_interval;
+	vdev_priv->num_reads = param->num_reads * 2;
+
+	if (vdev_priv->num_reads && vdev_priv->time_sync_interval) {
+		vdev_priv->num_reads--;
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+		qdf_delayed_work_start(&vdev_priv->ftm_time_sync_work,
+				       param->timer_interval);
+	} else if (vdev_priv->time_sync_interval == 0) {
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+		vdev_priv->ftm_ts_priv.qtime_ref = param->qtime;
+		vdev_priv->ftm_ts_priv.mac_ref = param->mac_time;
+		qdf_delayed_work_stop_sync(&vdev_priv->ftm_time_sync_work);
+	} else {
+		qdf_mutex_release(&vdev_priv->ftm_time_sync_mutex);
+	}
+
+	ftm_timesync_vdev_put_ref(vdev);
+
+	return QDF_STATUS_SUCCESS;
+}
+
+QDF_STATUS tgt_ftm_ts_offset_evt(struct wlan_objmgr_psoc *psoc,
+				 struct ftm_time_sync_offset *param)
+{
+	struct ftm_timesync_vdev_priv *vdev_priv;
+	struct wlan_objmgr_vdev *vdev;
+	uint8_t vdev_id;
+	int iter;
+
+	vdev_id = param->vdev_id;
+
+	vdev = wlan_objmgr_get_vdev_by_id_from_psoc(psoc, vdev_id,
+						    FTM_TIME_SYNC_ID);
+	if (!vdev) {
+		ftm_time_sync_err("failed to get vdev");
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	vdev_priv = ftm_timesync_vdev_get_priv(vdev);
+
+	vdev_priv->num_qtime_pair = param->num_qtime <
+			FTM_TIME_SYNC_QTIME_PAIR_MAX ? param->num_qtime :
+			FTM_TIME_SYNC_QTIME_PAIR_MAX;
+
+	for (iter = 0; iter < vdev_priv->num_qtime_pair; iter++) {
+		vdev_priv->ftm_ts_priv.time_pair[iter].qtime_master =
+						param->pairs[iter].qtime_master;
+		vdev_priv->ftm_ts_priv.time_pair[iter].qtime_slave =
+						param->pairs[iter].qtime_slave;
+	}
+
+	ftm_timesync_vdev_put_ref(vdev);
+
+	return QDF_STATUS_SUCCESS;
+}
+