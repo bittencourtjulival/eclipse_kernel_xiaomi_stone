MiCode/vendor_qcom_opensource_wlan

qcacmn: Populate additional params to CFR info for QCA6750
HAL changes to populate agc gain, CFO, rx_start_ts, mcs_rate and
gi_type to CFR info for QCA6750.

Change-Id: I164324b1e929399a8dacf88f3012970c65d5d653
CRs-Fixed: 3115256

target_if/cfr/src/target_if_cfr.c
@@ -247,6 +247,7 @@ void target_if_cfr_fill_header(struct csi_cfr_header *hdr,
 		if (target_type == TARGET_TYPE_QCN9000 ||
 		    target_type == TARGET_TYPE_QCN9224 ||
 		    target_type == TARGET_TYPE_QCA6490 ||
+		    target_type == TARGET_TYPE_QCA6750 ||
 		    target_type == TARGET_TYPE_KIWI)
 			hdr->cmn.cfr_metadata_version = CFR_META_VERSION_7;
 		else if ((target_type == TARGET_TYPE_QCA6018) ||

bittencourtjulival/eclipse_kernel_xiaomi_stone

target_if/cfr/src/target_if_cfr.c
@@ -227,7 +227,8 @@ void target_if_cfr_fill_header(struct csi_cfr_header *hdr,
 		hdr->cmn.chip_type = CFR_CAPTURE_RADIO_HKV2;
 	} else {
 		if (target_type == TARGET_TYPE_QCN9000 ||
-		    target_type == TARGET_TYPE_QCA6490)
+		    target_type == TARGET_TYPE_QCA6490 ||
+		    target_type == TARGET_TYPE_QCA6750)
 			hdr->cmn.cfr_metadata_version = CFR_META_VERSION_7;
 		else if ((target_type == TARGET_TYPE_QCA6018) ||
 			 ((target_type == TARGET_TYPE_QCA5018) && (!is_rcc)))