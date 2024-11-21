657c39acf25603bfef9985f204902a73baf5ad41
Amrito Das / <bhabanidas431@gmail.com>
2024-11-21 15:02:53 +0530

    Add Build Script
    Signed-off-by: Amrito | アンブロシア <amrito@project-pixelstar.xyz>

---
+++ build.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#set -e
+
+## Copy this script inside the kernel directory
+KERNEL_DEFCONFIG=veux_defconfig
+ANYKERNEL3_DIR=$PWD/AnyKernel3/
+FINAL_KERNEL_ZIP=Paimon-Kernel-veux-$(date '+%Y%m%d').zip
+export ARCH=arm64
+export KBUILD_BUILD_HOST=Taki
+export KBUILD_BUILD_USER=GoogleFucksYou
+export KBUILD_COMPILER_STRING="$($HOME/clang-r510928/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export PATH="$HOME/clang-r510928/bin:$PATH"
+
+# clone clang
+if ! [ -d "$HOME/clang-r510928" ]; then
+echo "Clang not found! Cloning..."
+if ! git clone -q https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-playground -b 17 --depth=1 --single-branch ~/clang-r510928; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+# Speed up build process
+MAKE="./makeparallel"
+
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+# Clean build always lol
+echo "**** Cleaning ****"
+mkdir -p out
+make O=out clean
+make mrproper
+
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "$blue***********************************************"
+echo "          BUILDING KERNEL          "
+echo -e "***********************************************$nocol"
+make $KERNEL_DEFCONFIG O=out
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      LD=ld.lld \
+                      LLVM=1 \
+                      LLVM_IAS=1
+
+echo "**** Verify Image.gz, dtbo.img , dtb ****"
+ls $PWD/out/arch/arm64/boot/Image
+ls $PWD/out/arch/arm64/boot/dtbo.img
+ls $PWD/out/arch/arm64/boot/dts/vendor/xiaomi/peux.dtb
+
+# Anykernel 3 time!!
+echo "**** Verifying AnyKernel3 Directory ****"
+ls $ANYKERNEL3_DIR
+echo "**** Removing leftovers ****"
+rm -rf $ANYKERNEL3_DIR/Image
+rm -rf $ANYKERNEL3_DIR/dtbo.img
+rm -rf $ANYKERNEL3_DIR/dtb
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+
+echo "**** Copying Image.gz , dtbo.img & dtb ****"
+cp $PWD/out/arch/arm64/boot/Image $ANYKERNEL3_DIR/
+cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/
+cp $PWD/out/arch/arm64/boot/dts/vendor/xiaomi/peux.dtb $ANYKERNEL3_DIR/dtb
+
+echo "**** Time to zip up! ****"
+cd $ANYKERNEL3_DIR/
+zip -r9 "../$FINAL_KERNEL_ZIP" * -x README $FINAL_KERNEL_ZIP
+
+echo "**** Done, here is your sha1 ****"
+cd ..
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+rm -rf $ANYKERNEL3_DIR/Image
+rm -rf $ANYKERNEL3_DIR/dtbo.img
+rm -rf $ANYKERNEL3_DIR/dtb
+rm -rf out/
+rm -rf clang-llvm
+rm -rf weebx-clang.tar.gz
+
+sha1sum $FINAL_KERNEL_ZIP
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+post_msg="Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+curl -v -F chat_id=$chat_id -F document=@$FINAL_KERNEL_ZIP -F caption="$post_msg" https://api.telegram.org/bot$token/sendDocument
