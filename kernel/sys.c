05ab87313ffb18bebe6bfc72205e95adcdb5e4e2
Thomas Turner <thomas_turner@talktalk.net>
2025-07-17 19:17:00 +0100

    uname: Add support for fake uname
    To make this patch more maintainable we will add
    support from 5.4 all the way to 6.12, and also
    enable 6.12 since the chain above adds support
    for it.
    
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1240,6 +1240,22 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+#ifndef CONFIG_FAKE_UNAME_NONE
+#if defined(CONFIG_FAKE_UNAME_5_4)
+#define FAKE_UNAME "5.4.296"
+#elif defined(CONFIG_FAKE_UNAME_5_10)
+#define FAKE_UNAME "5.10.241"
+#elif defined(CONFIG_FAKE_UNAME_5_15)
+#define FAKE_UNAME "5.15.190"
+#elif defined(CONFIG_FAKE_UNAME_6_1)
+#define FAKE_UNAME "6.1.149"
+#elif defined(CONFIG_FAKE_UNAME_6_6)
+#define FAKE_UNAME "6.6.103"
+#elif defined(CONFIG_FAKE_UNAME_6_12)
+#define FAKE_UNAME "6.12.44"
+#endif
+#endif
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1248,6 +1264,16 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifndef CONFIG_FAKE_UNAME_NONE
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
+		strcpy(tmp.release, FAKE_UNAME);
+		pr_debug("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
+#endif
 	up_read(&uts_sem);
 
 	rcu_read_lock();
