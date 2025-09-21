b0a4e4eb75d145fb1b3edb9fc777ce4092bc32f3
Giovanni Ricca <giovanniricca@proton.me>
2025-09-21 17:28:44 +0200

    input: touchscreen: focaltech_touch: Register AVDD gpio
    Change-Id: Ied68b18f155fded089b4531d159cf384e9ef2a76

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -1320,7 +1320,11 @@ int fts_reset_proc(int hdelayms)
 {
 	FTS_DEBUG("tp reset");
 	gpio_direction_output(fts_data->pdata->reset_gpio, 0);
-	msleep(1);
+	msleep(2);
+	gpio_direction_output(fts_data->pdata->avdd_gpio, 0);
+	msleep(10);
+	gpio_direction_output(fts_data->pdata->avdd_gpio, 1);
+	msleep(2);
 	gpio_direction_output(fts_data->pdata->reset_gpio, 1);
 	if (hdelayms) {
 		msleep(hdelayms);
@@ -2178,6 +2182,7 @@ static int fts_power_source_ctrl(struct fts_ts_data *ts_data, int enable)
 			FTS_DEBUG("regulator enable !");
 			gpio_direction_output(ts_data->pdata->reset_gpio, 0);
 			msleep(1);
+			gpio_direction_output(ts_data->pdata->avdd_gpio, 1);
 			ret = fts_ts_enable_reg(ts_data, true);
 			if (ret)
 				FTS_ERROR("Touch reg enable failed\n");
@@ -2188,6 +2193,7 @@ static int fts_power_source_ctrl(struct fts_ts_data *ts_data, int enable)
 			FTS_DEBUG("regulator disable !");
 			gpio_direction_output(ts_data->pdata->reset_gpio, 0);
 			msleep(1);
+			gpio_direction_output(ts_data->pdata->avdd_gpio, 0);
 			ret = fts_ts_enable_reg(ts_data, false);
 			if (ret)
 				FTS_ERROR("Touch reg disable failed");
@@ -2357,9 +2363,27 @@ static int fts_gpio_configure(struct fts_ts_data *data)
 		}
 	}
 
+	/* request avdd gpio */
+	if (gpio_is_valid(data->pdata->avdd_gpio)) {
+		ret = gpio_request(data->pdata->avdd_gpio, "fts_avdd_gpio");
+		if (ret) {
+			FTS_ERROR("[GPIO]avdd gpio request failed");
+			goto err_reset_gpio_dir;
+		}
+
+		ret = gpio_direction_output(data->pdata->avdd_gpio, 1);
+		if (ret) {
+			FTS_ERROR("[GPIO]set_direction for avdd gpio failed");
+			goto err_avdd_gpio_dir;
+		}
+	}
+
 	FTS_FUNC_EXIT();
 	return 0;
 
+err_avdd_gpio_dir:
+	if (gpio_is_valid(data->pdata->avdd_gpio))
+		gpio_free(data->pdata->avdd_gpio);
 err_reset_gpio_dir:
 	if (gpio_is_valid(data->pdata->reset_gpio))
 		gpio_free(data->pdata->reset_gpio);
@@ -2464,6 +2488,12 @@ static int fts_parse_dt(struct device *dev, struct fts_ts_platform_data *pdata)
 			0, &pdata->reset_gpio_flags);
 	if (pdata->reset_gpio < 0)
 		FTS_ERROR("Unable to get reset_gpio");
+
+	pdata->avdd_gpio = of_get_named_gpio_flags(np, "focaltech,avdd-gpio",
+			0, &pdata->avdd_gpio_flags);
+	FTS_INFO("avdd_gpio = %d", pdata->avdd_gpio);
+	if (pdata->avdd_gpio < 0)
+		FTS_DEBUG("Unable to get avdd_gpio");
 
 	pdata->irq_gpio = of_get_named_gpio_flags(np, "focaltech,irq-gpio",
 			0, &pdata->irq_gpio_flags);
@@ -2694,6 +2724,8 @@ tvm_setup:
 err_irq_req:
 	if (gpio_is_valid(fts_data->pdata->reset_gpio))
 		gpio_free(fts_data->pdata->reset_gpio);
+	if (gpio_is_valid(fts_data->pdata->avdd_gpio))
+		gpio_free(fts_data->pdata->avdd_gpio);
 	if (gpio_is_valid(fts_data->pdata->irq_gpio))
 		gpio_free(fts_data->pdata->irq_gpio);
 #if FTS_POWER_SOURCE_CUST_EN
@@ -2893,6 +2925,9 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
 
 	if (gpio_is_valid(ts_data->pdata->reset_gpio))
 		gpio_free(ts_data->pdata->reset_gpio);
+
+	if (gpio_is_valid(ts_data->pdata->avdd_gpio))
+		gpio_free(ts_data->pdata->avdd_gpio);
 
 	if (gpio_is_valid(ts_data->pdata->irq_gpio))
 		gpio_free(ts_data->pdata->irq_gpio);
