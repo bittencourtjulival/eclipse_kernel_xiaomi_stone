8358841161eae6c97e6b17314890a627a1b4b3e0
bittencourtjulival <julival.bitencourt@gmail.com>
2025-11-06 10:09:08 +0000

    Revert "drivers: staging: ksu: expose variables used by manual hooks always"
    This reverts commit 8c9966b48fad20e41b9c0a0736fea5190e601913.

--- drivers/staging/ksu/ksud.c
+++ drivers/staging/ksu/ksud.c
@@ -58,12 +58,11 @@ static void stop_input_hook();
 static struct work_struct stop_vfs_read_work;
 static struct work_struct stop_execve_hook_work;
 static struct work_struct stop_input_hook_work;
-#endif
-
+#else
 bool ksu_vfs_read_hook __read_mostly = true;
 bool ksu_execveat_hook __read_mostly = true;
 bool ksu_input_hook __read_mostly = true;
-
+#endif
 
 u32 ksu_devpts_sid;
 
