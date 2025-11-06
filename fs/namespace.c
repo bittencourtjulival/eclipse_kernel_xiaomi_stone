ffca29e5a5e7b54f0f09e6fcada0170d1b58c465
bittencourtjulival <julival.bitencourt@gmail.com>
2025-11-06 09:53:33 +0000

    Revert "fs: fix a struct path leak in path_umount"
    This reverts commit 80a39ff448f573e7a10a631892868db04bcdc45f.

--- fs/namespace.c
+++ fs/namespace.c
@@ -1853,38 +1853,34 @@ static inline bool may_mandlock(void)
 }
 #endif
 
-static int can_umount(const struct path *path, int flags)
+static int path_umount(struct path *path, int flags)
 {
-	struct mount *mnt = real_mount(path->mnt);
+	struct mount *mnt;
+	int retval;
 
 	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
 		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
+
+	mnt = real_mount(path->mnt);
+	retval = -EINVAL;
 	if (path->dentry != path->mnt->mnt_root)
-		return -EINVAL;
+		goto dput_and_out;
 	if (!check_mnt(mnt))
-		return -EINVAL;
+		goto dput_and_out;
 	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
-		return -EINVAL;
+		goto dput_and_out;
+	retval = -EPERM;
 	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		return -EPERM;
-	return 0;
-}
+		goto dput_and_out;
 
-int path_umount(struct path *path, int flags)
-{
-	struct mount *mnt = real_mount(path->mnt);
-	int ret;
-
-	ret = can_umount(path, flags);
-	if (!ret)
-		ret = do_umount(mnt, flags);
-
+	retval = do_umount(mnt, flags);
+dput_and_out:
 	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
 	dput(path->dentry);
 	mntput_no_expire(mnt);
-	return ret;
+	return retval;
 }
 
 int ksys_umount(char __user *name, int flags)
