d22bb612f78edf73acd672743279e11b0f9f8079
bittencourtjulival <julival.bitencourt@gmail.com>
2025-11-06 10:09:13 +0000

    Revert "drivers: staging: ksu: guard functionality that requires kprobes"
    This reverts commit db628de98dd23bb25cecb1b73398714284b0b1a8.

--- drivers/staging/ksu/core_hook.c
+++ drivers/staging/ksu/core_hook.c
@@ -941,7 +941,7 @@ void __init ksu_core_init(void)
 
 void ksu_core_exit(void)
 {
-#ifdef CONFIG_KSU_WITH_KPROBES
+#ifdef CONFIG_KPROBES
 	pr_info("ksu_core_kprobe_exit\n");
 	// we dont use this now
 	// ksu_kprobe_exit();
