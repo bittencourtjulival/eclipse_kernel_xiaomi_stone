e91d9f1478617464de805d5607c039174569a2f6
Gabor Juhos <j4g8y7@gmail.com>
2025-11-07 18:10:08 +0100

    regulator: core: disable supply if enabling main regulator fails
    [ Upstream commit fb1ebb10468da414d57153ddebaab29c38ef1a78 ]
    
    For 'always-on' and 'boot-on' regulators, the set_machine_constraints()
    may enable supply before enabling the main regulator, however if the
    latter fails, the function returns with an error but the supply remains
    enabled.
    
    When this happens, the regulator_register() function continues on the
    error path where it puts the supply regulator. Since enabling the supply
    is not balanced with a disable call, a warning similar to the following
    gets issued from _regulator_put():
    
        [    1.603889] WARNING: CPU: 2 PID: 44 at _regulator_put+0x8c/0xa0
        [    1.603908] Modules linked in:
        [    1.603926] CPU: 2 UID: 0 PID: 44 Comm: kworker/u16:3 Not tainted 6.18.0-rc4 #0 NONE
        [    1.603938] Hardware name: Qualcomm Technologies, Inc. IPQ9574/AP-AL02-C7 (DT)
        [    1.603945] Workqueue: async async_run_entry_fn
        [    1.603958] pstate: 60400005 (nZCv daif +PAN -UAO -TCO -DIT -SSBS BTYPE=--)
        [    1.603967] pc : _regulator_put+0x8c/0xa0
        [    1.603976] lr : _regulator_put+0x7c/0xa0
        ...
        [    1.604140] Call trace:
        [    1.604145]  _regulator_put+0x8c/0xa0 (P)
        [    1.604156]  regulator_register+0x2ec/0xbf0
        [    1.604166]  devm_regulator_register+0x60/0xb0
        [    1.604178]  rpm_reg_probe+0x120/0x208
        [    1.604187]  platform_probe+0x64/0xa8
        ...
    
    In order to avoid this, change the set_machine_constraints() function to
    disable the supply if enabling the main regulator fails.
    
    Fixes: 05f224ca6693 ("regulator: core: Clean enabling always-on regulators + their supplies")
    Signed-off-by: Gabor Juhos <j4g8y7@gmail.com>
    Link: https://patch.msgid.link/20251107-regulator-disable-supply-v1-1-c95f0536f1b5@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 1eb2827da3f408f6fb72ce497fc0665d3967bf37)
    
    Conflicts:
    	drivers/regulator/core.c
    Notes: Ignore context change. conflicts are caused by missing commit
    61aab5ad27d5 ("regulator: print symbolic errors in kernel messages").
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1476,6 +1476,8 @@ static int set_machine_constraints(struct regulator_dev *rdev)
 	 * and we have control then make sure it is enabled.
 	 */
 	if (rdev->constraints->always_on || rdev->constraints->boot_on) {
+		bool supply_enabled = false;
+
 		/* If we want to enable this regulator, make sure that we know
 		 * the supplying regulator.
 		 */
@@ -1495,11 +1497,14 @@ static int set_machine_constraints(struct regulator_dev *rdev)
 				rdev->supply = NULL;
 				return ret;
 			}
+			supply_enabled = true;
 		}
 
 		ret = _regulator_do_enable(rdev);
 		if (ret < 0 && ret != -EINVAL) {
 			rdev_err(rdev, "failed to enable\n");
+			if (supply_enabled)
+				regulator_disable(rdev->supply);
 			return ret;
 		}
 
