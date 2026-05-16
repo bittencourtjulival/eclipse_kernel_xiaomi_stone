6d97f890a7fbe4b7b666b7581118a15811b8cc83
spuligil <spuligil@qti.qualcomm.com>
2026-05-15 18:02:00 -0700

    fw-api: CL 32544807 - update fw common interface files
    Change-Id: Id45d1786ca4116b28a367b3705928a9e434e99a8
    CRs-Fixed: 3830439

--- drivers/staging/fw-api/fw/wmi_unified.h
+++ drivers/staging/fw-api/fw/wmi_unified.h
@@ -18391,6 +18391,10 @@ enum WMI_VDEV_UP_FLAGS {
      * Valid only for VBSS subtype vaps.
      */
     WMI_VDEV_UP_FLAG_VBSS_PASSIVE  = 0x00000004,
+    /** REDUCE_OFF_DIAGONAL_NULL_THR
+     * The flag will be set to reduce off diagonal nulling threshold.
+     */
+    WMI_VDEV_UP_FLAG_REDUCE_OFF_DIAGONAL_NULL_THR = 0x00000008,
 };
 
 typedef struct{
