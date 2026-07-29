b5025e854dae13540bbb836207dbd25280ea55e7
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-30 01:49:16 +0600

    qseecom: Skip freezer checks when TrustZone is busy
    When user space requests secure operations, qseecom makes a secure monitor call to
    Qualcomm's TrustZone. If tz is busy, the driver enters a retry loop inside
    __qseecom_scm_call2_locked() where it releases locks and sleeps for 30ms before
    retrying, for up to 2 seconds.
    
    But during this period, the task remains in kernel space. So, it never returns to
    user space to handle pending signals. At this window, if for any reason a suspend
    tries to occur, freezer fails to suspend the process for obvious reasons. This is
    visible from the following call trace:
    
    [10028.106135] Freezing of tasks failed after 1.009 seconds (1 tasks refusing to freeze, wq_busy=0):
    [10028.106259] task:keymaster@4.1-s state:R  running task     stack:0     pid:537   ppid:1      flags:0x04000009
    [10028.106278] Call trace:
    [10028.106293]  __switch_to+0x118/0x130
    [10028.106302]  pick_next_task_fair+0x2f4/0x5e0
    [10028.106311]  __schedule+0x240/0x7d0
    [10028.106318]  preempt_schedule_irq+0x4c/0x80
    [10028.106324]  0xffffff8d78903b68
    
    Fix this by wrapping the SCM retry loop with freezer_do_not_count() and
    freezer_count() to skip the freezer checks at this point. Freezer will know it's
    safe to skip this thread while it waits for a response from tz.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -478,6 +478,7 @@ static int __qseecom_scm_call2_locked(uint32_t smc_id, struct scm_desc *desc)
 	int ret = 0;
 	int retry_count = 0;
 
+	freezer_do_not_count();
 	do {
 		ret = qcom_scm_qseecom_call_noretry(smc_id, desc);
 		if ((ret == -EBUSY) || (desc && (desc->ret[0] == -QSEE_RESULT_FAIL_APP_BUSY))) {
@@ -489,6 +490,8 @@ static int __qseecom_scm_call2_locked(uint32_t smc_id, struct scm_desc *desc)
 			pr_warn("secure world has been busy for 1 second!\n");
 	} while (((ret == -EBUSY) || (desc && (desc->ret[0] == -QSEE_RESULT_FAIL_APP_BUSY))) &&
 			(retry_count++ < QSEECOM_SCM_EBUSY_MAX_RETRY));
+	freezer_count();
+
 	return ret;
 }
 
